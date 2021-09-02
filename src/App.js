45e6b9894fc192acfe1118db27519cee1d756dc9
AnuragCool <anuragcooldavkh@gmal.com>
2021-09-02 23:50:38 +0530

    created board and square components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from './components/Board'
+
+function App(){
+  return (
+    <>
+    <h1>TIC TAC TOE</h1>
+    <Board/>
+    </>
+  )
+}
+
+export default App;
